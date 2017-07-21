166066c21cf891221bd14bb2b356a0f62c77a2e9
Giuseppe <giuscri@gmail.com>
2017-07-21 22:59:47 +0200

    Write solution for the "SVM" module.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,40 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+n = len(features_train)
+#features_train = features_train[:int(n/100)]
+#labels_train = labels_train[:int(n/100)]
+
+C = 10000.
+clf = SVC(kernel='rbf', C=C)
+
+start = time()
+clf.fit(features_train, labels_train)
+print('[C={}] SVC took {} ms to train'.format(C, int((time()-start)*1000)))
+
+start = time()
+labels_test_predicted = clf.predict(features_test)
+print('[C={}] SVC took {} ms to make test predictions'.format(C, int((time()-start)*1000)))
+
+score = accuracy_score(labels_test, labels_test_predicted)
+print('[C={}] score for SVC is {:.2f}'.format(C, score))
+
+p0, p1, p2 = labels_test_predicted[10, 26, 50]
+print('{}, {}, {}', p0, p1, p2)
+#for C in (10., 1000., 100., 10000.):
+#    clf = SVC(kernel='rbf', C=C)
+#    clf.fit(features_train, labels_train)
+#    print('[C={}] SVC took {} ms to train'.format(C, int((time()-start)*1000)))
+#
+#    start = time()
+#    labels_test_predicted = clf.predict(features_test)
+#    print('[C={}] SVC took {} ms to make test predictions'.format(C, int((time()-start)*1000)))
+#
+#    score = accuracy_score(labels_test, labels_test_predicted)
+#    print('[C={}] score for SVC is {:.2f}'.format(C, score))
 #########################################################
 
 
